35a1ef7143534286133016d3c3680c11c7af2afe
Bit Second <bitsecondal@gmail.com>
2018-01-14 21:28:30 -0600

    Removed shell scripts

--- get_weather_config_ex.sh
+++
@@ -1,9 +0,0 @@
-# Enter the file system path to the location that Drupal will pull feeds from
-feedsdir=/path/to/drupal/feeds/location
-
-# Enter the NWS location code that you want to be queried
-# Location code can be found by going to http://w1.weather.gov/xml/current_obs/ and searching for your location. 
-location=ABCD
-
-export feedsdir
-export location
